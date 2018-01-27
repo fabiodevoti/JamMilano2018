4c5b80fc47bce29d42e8405b47044bd0b5e0598b
PCdiGioele\Gioele <kleon@hotmail.it>
2018-01-27 09:13:01 +0100

    aggiunta di enemy senza AI e Altra camera

--- GGJ2018/Assets/Lior/Scripts/CameraFollow.cs
+++ GGJ2018/Assets/Lior/Scripts/CameraFollow.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour 
+/*public class CameraFollow : MonoBehaviour
 {
-	public Transform target;
+    public Transform target;
 
 	public float smoothSpeed = 0.125f;
 	public Vector3 offset;
@@ -12,9 +12,43 @@ public class CameraFollow : MonoBehaviour
 	void LateUpdate ()
 	{
 		Vector3 desiredPosition = target.position + offset;
-		Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-		transform.position = smoothedPosition;
-
-		transform.LookAt(target);
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+        transform.LookAt(target);
 	}
-}
+}*/
+
+public class CameraFollow : MonoBehaviour
+    {
+
+        public float interpVelocity;
+        public float minDistance;
+        public float followDistance;
+        public GameObject target;
+        public Vector3 offset;
+        Vector3 targetPos;
+        // Use this for initialization
+        void Start()
+        {
+            targetPos = transform.position;
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            if (target)
+            {
+                Vector3 posNoZ = transform.position;
+                posNoZ.z = target.transform.position.z;
+
+                Vector3 targetDirection = (target.transform.position - posNoZ);
+
+                interpVelocity = targetDirection.magnitude * 5f;
+
+                targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime);
+
+                transform.position = Vector3.Lerp(transform.position, targetPos + offset, 0.25f);
+
+            }
+        }
+    }
