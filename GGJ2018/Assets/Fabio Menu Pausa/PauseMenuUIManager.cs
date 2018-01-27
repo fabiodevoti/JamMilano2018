2c64331ebfb4344e3315574449aeaf1ad3958345
DEVOTI-SURFACE\fabio <fabio.devoti@gmail.com>
2018-01-27 17:01:11 +0100

    Main Menu & Pause Menu

--- GGJ2018/Assets/Fabio Menu Pausa/PauseMenuUIManager.cs
+++ GGJ2018/Assets/Fabio Menu Pausa/PauseMenuUIManager.cs
@@ -10,13 +10,39 @@ public class PauseMenuUIManager : MonoBehaviour
     public GameObject pausePanel;
     public GameObject backToGame;
     public GameObject backToMainMenu;
+    public GameObject controllerImage;
+    public EventSystem eventSystem; // The Event System
+    public GameObject firstSelectedPauseMenuButton; // First button selected in the Main Menu
+    public GameObject firstSelectedControllerMenuButton; // First button selected in the "Controller Menu"
+    public GameObject firstSelectedExitMenuButton; // First button selected in the "Security Menu"
+    public Button areYouSureYesButton; // The "Yes Button" in the "Security Menu"
+    public Button areYouSureNoButton; // The "No Button" in the "Security Menu"
+    public Button backToPauseMenuButton; // The button that allow to disable the "controller image" and allow to go to the main menu
 
     void Start()
     { }
 
-    public void PausePanelEnable()
+    public void PausePanelEnable() // Enable the pause panel
     {
         pausePanel.gameObject.SetActive(true);
+        eventSystem.SetSelectedGameObject(firstSelectedPauseMenuButton);
+        backToGame.gameObject.SetActive(true);
+        backToMainMenu.gameObject.SetActive(true);
+        controllerImage.gameObject.SetActive(false);
+        areYouSureYesButton.gameObject.SetActive(false);
+        areYouSureNoButton.gameObject.SetActive(false);
+        backToPauseMenuButton.gameObject.SetActive(false);
+    }
+
+    public void ControllerImage() // Enabled the Controller Image
+    {
+        backToGame.gameObject.SetActive(false);
+        backToMainMenu.gameObject.SetActive(false);
+        controllerImage.gameObject.SetActive(true);
+        areYouSureYesButton.gameObject.SetActive(false);
+        areYouSureNoButton.gameObject.SetActive(false);
+        backToPauseMenuButton.gameObject.SetActive(true);
+        eventSystem.SetSelectedGameObject(firstSelectedControllerMenuButton);   
     }
 
     public void BackToGame ()
@@ -24,8 +50,35 @@ public class PauseMenuUIManager : MonoBehaviour
         pausePanel.gameObject.SetActive(false);
     }
 
-    public void BackToMainMenu ()
+    public void BackToMainMenuChoice ()
+    {
+        backToGame.gameObject.SetActive(true);
+        backToMainMenu.gameObject.SetActive(true);
+        controllerImage.gameObject.SetActive(false);
+        areYouSureYesButton.gameObject.SetActive(false);
+        areYouSureNoButton.gameObject.SetActive(false);
+        backToPauseMenuButton.gameObject.SetActive(false);
+        eventSystem.SetSelectedGameObject(firstSelectedExitMenuButton);
+    }
+
+    public void AreYouSureYes ()
     {
         //SceneManager.LoadSceneAsync(1);
+    }
+
+    public void FadesMenu(string menuType) // Set the time to wait until the fade animation is finished
+    {
+        Invoke("MainMenu", 0.2f);
+    }
+
+    public void AreYouSureNo ()
+    {
+        eventSystem.SetSelectedGameObject(firstSelectedPauseMenuButton);
+        backToGame.gameObject.SetActive(true);
+        backToMainMenu.gameObject.SetActive(true);
+        controllerImage.gameObject.SetActive(false);
+        areYouSureYesButton.gameObject.SetActive(false);
+        areYouSureNoButton.gameObject.SetActive(false);
+        backToPauseMenuButton.gameObject.SetActive(false);
     }
 }
--- GGJ2018/Assets/Fabio Menu Principale/MenuUIManager.cs
+++ GGJ2018/Assets/Fabio Menu Principale/MenuUIManager.cs
@@ -7,21 +7,15 @@ using UnityEngine.EventSystems;
 
 public class MenuUIManager : MonoBehaviour
 {
-    public GameObject controllerImage; // Controller Image
     public EventSystem eventSystem; // The Event System
     public GameObject firstSelectedMainMenuButton; // First button selected in the Main Menu
-    public GameObject firstSelectedExitMenuButton; // First button selected in the "Security Menu"
-    public GameObject firstSelectedControllerMenuButton; // First button selected in the "Controller Menu"
-    public Button areYouSureYesButton; // The "Yes Button" in the "Security Menu"
-    public Button areYouSureNoButton; // The "No Button" in the "Security Menu"
-    public Button mainMenuButton; // The button that allow to disable the "controller image" and allow to go to the main menu
+    public Button startGameButton; // The "Yes Button" in the "Security Menu"
+    public Button exitGameButton; // The button that allow to disable the "controller image" and allow to go to the main menu
 
 	void Start() //Disable all the GameObject-Menu that has not to be on the screen
     {
-        controllerImage.gameObject.SetActive(false);
-        areYouSureYesButton.gameObject.SetActive(false);
-        areYouSureNoButton.gameObject.SetActive(false);
-        mainMenuButton.gameObject.SetActive(false);
+        startGameButton.gameObject.SetActive(true);
+        exitGameButton.gameObject.SetActive(true);
         eventSystem.SetSelectedGameObject(firstSelectedMainMenuButton);
     }
 
@@ -30,40 +24,8 @@ public class MenuUIManager : MonoBehaviour
 		//SceneManager.LoadSceneAsync(1);
 	}
 
-	public void FadesMenu(string menuType) // Set the time to wait until the fade animation is finished
-    {
-		Invoke("MainMenu", 0.2f);
-	}
-
-   public void ControllerImage() // Enabled the Controller Image
-    {
-        controllerImage.gameObject.SetActive(true);
-        areYouSureYesButton.gameObject.SetActive(false);
-        areYouSureNoButton.gameObject.SetActive(false);
-        mainMenuButton.gameObject.SetActive(true);
-        eventSystem.SetSelectedGameObject(firstSelectedControllerMenuButton);
-    }
-
-    public void ExitGameMenu() // Enabled the Exit Game Menu and disable all the others
-    {
-        controllerImage.gameObject.SetActive(false);
-        areYouSureYesButton.gameObject.SetActive(true);
-        areYouSureNoButton.gameObject.SetActive(true);
-        mainMenuButton.gameObject.SetActive(false);
-        eventSystem.SetSelectedGameObject(firstSelectedExitMenuButton); // Set the first selected button in the menu
-    }
-
-    public void ExitGameYes() // Exit game
+    public void ExitGame() // Exit game
     {
         Application.Quit();
-    }
-
-    public void AreYouSureNo() // Back to the main menu
-    {
-        controllerImage.gameObject.SetActive(false);
-        areYouSureYesButton.gameObject.SetActive(false);
-        areYouSureNoButton.gameObject.SetActive(false);
-        mainMenuButton.gameObject.SetActive(false);
-        eventSystem.SetSelectedGameObject(firstSelectedMainMenuButton);
     }
 }
