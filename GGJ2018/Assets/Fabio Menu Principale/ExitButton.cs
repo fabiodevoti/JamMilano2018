9a18561fe0ffef8bb08207caaa243483c63f5b65
DEVOTI-SURFACE\fabio <fabio.devoti@gmail.com>
2018-01-27 20:10:00 +0100

    Script vari

---
+++ GGJ2018/Assets/Fabio Menu Principale/ExitButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ExitButton : MonoBehaviour, ISelectHandler// required interface when using the OnSelect method.
+{
+    public GameObject exitSymbol;
+    //Do this when the selectable UI object is selected.
+    public void OnSelect(BaseEventData eventData)
+    {
+        Debug.Log("sono illuminato");
+    }
+}
