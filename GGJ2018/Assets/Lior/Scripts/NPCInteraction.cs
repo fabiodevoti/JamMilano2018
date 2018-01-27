a0c2df53dc94c2a05c1cda4af22def0369e21b66
liorvisbal <liorvisbal@gmail.com>
2018-01-27 21:22:32 +0100

    npc interaction safe

--- GGJ2018/Assets/Lior/Scripts/NPCInteraction.cs
+++ GGJ2018/Assets/Lior/Scripts/NPCInteraction.cs
@@ -2,74 +2,95 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPCInteraction : MonoBehaviour 
+public class NPCInteraction : MonoBehaviour
 {
 	public bool isInfected;
-	public float reactionTime = 1f;
 	public GameObject player;
+	public bool isCured = false;
+	public float reactionTime = 1f;
+	public float chargeUpTime = 1f;
 
-	private bool playerCollision;
+	public float chargeBar = 0f;
+
+	private bool playerCollision;    // Player in range to show audio & visual cue
 	private float timer;
+
 
 	void Update()
 	{
-		if (isInfected == true) 
+		if (playerCollision == true)
 		{
-			if (playerCollision == true) 
+			if (isInfected == true)
 			{
-				if (timer >= reactionTime) 
+				if (timer >= reactionTime)
 				{
 					player.GetComponent<PlayerMovement> ().isDead = true;
-				} 
-				else if (timer < reactionTime) 
+				}
+				else if (timer < reactionTime)
 				{
 					timer += Time.deltaTime;
-				} 
-			} 
-			else 
-			{
-				timer = 0f;
+				}
 			}
+			else if (Input.GetKey(KeyCode.Space) && isCured == false)
+			{
+				if (chargeBar >= chargeUpTime)
+				{
+					//TODO delete charge bar animation
+					isCured = true;
+					StartCoroutine(ChangeColour(0.5f));
+				}
+				else
+				{
+					chargeBar += Time.deltaTime;
+				}
+
+			}
+		}
+		else
+		{
+			timer = 0f;
+			// TODO delete charge bar animation IF IT EXISTS!!!!
+			chargeBar = 0f;
 		}
 	}
 	void OnTriggerEnter2D(Collider2D other)
 	{
-		if (other.GetComponent<Collider2D> ().tag == "Player") 
+		if (other.GetComponent<Collider2D>().tag == "Player")
 		{
 			playerCollision = true;
 
-			Debug.Log ("works");
-			StartCoroutine (ChangeColour(0.5f));
+			StartCoroutine(ChangeColour(0.5f));
 
 			/*if(Input.GetKeyDown(KeyCode.Space))
 			{
 			}*/
-		} 
+		}
 	}
 
 	void OnTriggerExit2D(Collider2D other)
 	{
-		if (other.GetComponent<Collider2D> ().tag == "Player") 
+		if (other.GetComponent<Collider2D>().tag == "Player")
 		{
 			playerCollision = false;
-		} 
+		}
 	}
 
 	public IEnumerator ChangeColour(float seconds)
 	{
-		if (isInfected == true) 
+		if (isInfected == true)
 		{
-			gameObject.GetComponent<SpriteRenderer> ().color = Color.yellow;
-		} 
-		else if (isInfected == false) 
+			gameObject.GetComponent<SpriteRenderer>().color = Color.yellow;
+		}
+		else if (isInfected == false && isCured == true)
 		{
-			gameObject.GetComponent<SpriteRenderer> ().color = Color.blue;
+			Debug.Log(isCured);
+			gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
 		}
 
 		yield return (new WaitForSeconds(seconds));
 
 
-		gameObject.GetComponent<SpriteRenderer> ().color = Color.white;
-			
+		gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+
 	}
- }
+}
