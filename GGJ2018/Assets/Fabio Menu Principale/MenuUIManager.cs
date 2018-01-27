1621dbb8f6c64362f74d3ad7f5597c7e67b99579
DEVOTI-SURFACE\fabio <fabio.devoti@gmail.com>
2018-01-27 09:43:53 +0100

    Menu e pausa senza fade

--- GGJ2018/Assets/Fabio Menu Principale/MenuUIManager.cs
+++ GGJ2018/Assets/Fabio Menu Principale/MenuUIManager.cs
@@ -8,18 +8,20 @@ using UnityEngine.EventSystems;
 public class MenuUIManager : MonoBehaviour
 {
     public GameObject controllerImage; // Controller Image
-    public GameObject areYouSure; // Security menu which appear only if there is a save file. In case of missing save file, it's doesn't appear
     public EventSystem eventSystem; // The Event System
     public GameObject firstSelectedMainMenuButton; // First button selected in the Main Menu
     public GameObject firstSelectedExitMenuButton; // First button selected in the "Security Menu"
-	public Button areYouSureYesButton; // The "Yes Button" in the "Security Menu"
+    public GameObject firstSelectedControllerMenuButton; // First button selected in the "Controller Menu"
+    public Button areYouSureYesButton; // The "Yes Button" in the "Security Menu"
     public Button areYouSureNoButton; // The "No Button" in the "Security Menu"
+    public Button mainMenuButton; // The button that allow to disable the "controller image" and allow to go to the main menu
 
 	void Start() //Disable all the GameObject-Menu that has not to be on the screen
     {
         controllerImage.gameObject.SetActive(false);
         areYouSureYesButton.gameObject.SetActive(false);
         areYouSureNoButton.gameObject.SetActive(false);
+        mainMenuButton.gameObject.SetActive(false);
         eventSystem.SetSelectedGameObject(firstSelectedMainMenuButton);
     }
 
@@ -38,6 +40,8 @@ public class MenuUIManager : MonoBehaviour
         controllerImage.gameObject.SetActive(true);
         areYouSureYesButton.gameObject.SetActive(false);
         areYouSureNoButton.gameObject.SetActive(false);
+        mainMenuButton.gameObject.SetActive(true);
+        eventSystem.SetSelectedGameObject(firstSelectedControllerMenuButton);
     }
 
     public void ExitGameMenu() // Enabled the Exit Game Menu and disable all the others
@@ -45,6 +49,7 @@ public class MenuUIManager : MonoBehaviour
         controllerImage.gameObject.SetActive(false);
         areYouSureYesButton.gameObject.SetActive(true);
         areYouSureNoButton.gameObject.SetActive(true);
+        mainMenuButton.gameObject.SetActive(false);
         eventSystem.SetSelectedGameObject(firstSelectedExitMenuButton); // Set the first selected button in the menu
     }
 
@@ -58,6 +63,7 @@ public class MenuUIManager : MonoBehaviour
         controllerImage.gameObject.SetActive(false);
         areYouSureYesButton.gameObject.SetActive(false);
         areYouSureNoButton.gameObject.SetActive(false);
+        mainMenuButton.gameObject.SetActive(false);
         eventSystem.SetSelectedGameObject(firstSelectedMainMenuButton);
     }
 }
