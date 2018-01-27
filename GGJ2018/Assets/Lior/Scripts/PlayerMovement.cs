1ef37238cf7767d5978193759af90ea2b0d21c10
liorvisbal <liorvisbal@gmail.com>
2018-01-27 02:42:40 +0100

    Movimenti di base, teletrasporto e bordi funzionanti

--- GGJ2018/Assets/Lior/Scripts/PlayerMovement.cs
+++ GGJ2018/Assets/Lior/Scripts/PlayerMovement.cs
@@ -2,15 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour 
+{
+	public float speed = 1f;
+	public float jumpForce = 1f;
+	public float downJumpForce = 1f;
+	public LayerMask layerMask;
+
+	private float gravityForce;
+	private Rigidbody2D rb;
+	private bool isUp = false;
+	private bool isBase = false;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D> ();
+		gravityForce = rb.velocity.y;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () 
+	{
+		HandleMovement ();
+		CheckBorders ();
+	}
+
+	void Update()
+	{
+		Jump ();
+	}
+
+	private void HandleMovement()
+	{
+		float inputH = Input.GetAxis ("Horizontal") * speed;
+		 
+
+		rb.velocity = new Vector2(inputH, rb.velocity.y) * Time.deltaTime;
+	}
+
+	private void Jump()
+	{
+		if ((Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown (KeyCode.UpArrow)) && isUp == false) 
+		{
+			transform.Translate (new Vector2(0, jumpForce * Time.deltaTime));
+		}
+		else if ((Input.GetKeyDown (KeyCode.S) || Input.GetKeyDown (KeyCode.DownArrow)) && isBase == false) 
+		{
+			transform.Translate (new Vector2(0, -jumpForce * Time.deltaTime));
+		} 
+	}
+
+	private void CheckBorders()
+	{
+		RaycastHit2D upHit = Physics2D.Raycast (transform.position, Vector2.up, 100f, layerMask);
+
+		if (upHit.collider.gameObject.tag == "Up Border") 
+		{
+			isUp = true;
+		} 
+		else 
+		{
+			isUp = false;
+		}
+			
+
+		RaycastHit2D baseHit = Physics2D.Raycast (transform.position, Vector2.down, 100f, layerMask);
+		Debug.Log (baseHit.collider.gameObject.tag);
+
+		if (baseHit.collider.gameObject.tag == "Base Border") 
+		{
+			isBase = true;
+		} 
+		else 
+		{
+			isBase = false;
+		}
 	}
 }
